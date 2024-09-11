b842e16da5b1e0234ee029258c91dd6ba93b4904
Djato <delpheandjato@gmail.com>
2024-09-11 18:00:22 +0000

    Data types

---
+++ DataTypes/DataTypes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ 
+    char grade = 'A'; // Single character
+    string phrase = "Microsoft Corporation"; // String of character 
+    int age = 34;
+    double gpa = -4.3;
+    bool isMale = true;
+
+    cout << grade << endl;
+
+    return 0;
+}
